55436f0f9014dd60fca22d684a6bedc714baa85c
Brian Pinto <41571012+brianpinto91@users.noreply.github.com>
2020-11-05 18:39:21 +0100

    add a route to respond for POST requests

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def home():
 @app.route("/about", methods=['GET'])
 def about():
     return flask.render_template("about.html")
+@app.route("/todo", methods=['POST'])
+def todo():
+    return "<p>To do: implement a webpage to respond to the post request</p>"
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)

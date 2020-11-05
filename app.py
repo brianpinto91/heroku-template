3e930ab5ab3d9bc322471aedce18e32be91a166f
Brian Pinto <41571012+brianpinto91@users.noreply.github.com>
2020-11-05 16:23:18 +0100

    create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask
+
+app = flask.Flask(__name__, template_folder="./templates")
+
+@app.route("/", methods=['GET'])
+@app.route("/home", methods=['GET'])
+def home():
+    return flask.render_template("home.html")
+
+@app.route("/about", methods=['GET'])
+def about():
+    return flask.render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
